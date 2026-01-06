0fdec1976605602ff7c0f814cb6d22cbf56655b6
ahmedmohamedrafat00-cpu <ahmed.mohamed.rafat.00@gmail.com>
2026-01-06 15:28:33 +0200

    add iam module for eks cluster and node groups

--- terraform/environments/dev/main.tf
+++ terraform/environments/dev/main.tf
@@ -7,3 +7,8 @@ module "vpc" {
   public_subnets  = ["10.0.1.0/24", "10.0.2.0/24"]
   private_subnets = ["10.0.101.0/24", "10.0.102.0/24"]
 }
+module "iam" {
+  source = "../../modules/iam"
+
+  project_name = local.project_name
+}
